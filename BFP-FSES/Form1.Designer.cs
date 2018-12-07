f62ed214a3209538ede2c5dc5526605c1c51fb3e
Basibas07 <basibaskarl28@gmail.com>
2018-12-07 13:54:09 +0800

    UI

--- BFP-FSES/Form1.Designer.cs
+++ BFP-FSES/Form1.Designer.cs
@@ -28,21 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Sidepanel1 = new System.Windows.Forms.Panel();
+            this.btnMASTERLIST = new System.Windows.Forms.Button();
+            this.btnREGISTER = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.Sidepanel2 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Sidepanel1
+            // 
+            this.Sidepanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(40)))), ((int)(((byte)(31)))));
+            this.Sidepanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Sidepanel1.Location = new System.Drawing.Point(0, 0);
+            this.Sidepanel1.Name = "Sidepanel1";
+            this.Sidepanel1.Size = new System.Drawing.Size(13, 55);
+            this.Sidepanel1.TabIndex = 0;
+            // 
+            // btnMASTERLIST
+            // 
+            this.btnMASTERLIST.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMASTERLIST.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMASTERLIST.FlatAppearance.BorderSize = 0;
+            this.btnMASTERLIST.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMASTERLIST.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMASTERLIST.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnMASTERLIST.Location = new System.Drawing.Point(13, 237);
+            this.btnMASTERLIST.Name = "btnMASTERLIST";
+            this.btnMASTERLIST.Size = new System.Drawing.Size(207, 55);
+            this.btnMASTERLIST.TabIndex = 3;
+            this.btnMASTERLIST.Text = "MASTERLIST";
+            this.btnMASTERLIST.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMASTERLIST.UseVisualStyleBackColor = true;
+            this.btnMASTERLIST.Click += new System.EventHandler(this.btnMASTERLIST_Click);
+            // 
+            // btnREGISTER
+            // 
+            this.btnREGISTER.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnREGISTER.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnREGISTER.FlatAppearance.BorderSize = 0;
+            this.btnREGISTER.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnREGISTER.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnREGISTER.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnREGISTER.Location = new System.Drawing.Point(13, 182);
+            this.btnREGISTER.Name = "btnREGISTER";
+            this.btnREGISTER.Size = new System.Drawing.Size(207, 55);
+            this.btnREGISTER.TabIndex = 2;
+            this.btnREGISTER.Text = "REGISTER";
+            this.btnREGISTER.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnREGISTER.UseVisualStyleBackColor = true;
+            this.btnREGISTER.Click += new System.EventHandler(this.btnREGISTER_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.Sidepanel2);
+            this.panel4.Controls.Add(this.Sidepanel1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 182);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(13, 424);
+            this.panel4.TabIndex = 1;
+            // 
+            // Sidepanel2
+            // 
+            this.Sidepanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Sidepanel2.Location = new System.Drawing.Point(0, 55);
+            this.Sidepanel2.Name = "Sidepanel2";
+            this.Sidepanel2.Size = new System.Drawing.Size(13, 55);
+            this.Sidepanel2.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(220, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(702, 606);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(55)))), ((int)(((byte)(70)))));
+            this.panel1.Controls.Add(this.btnMASTERLIST);
+            this.panel1.Controls.Add(this.btnREGISTER);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 606);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(220, 182);
+            this.panel3.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(679, 474);
+            this.ClientSize = new System.Drawing.Size(922, 606);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel4.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Sidepanel1;
+        private System.Windows.Forms.Button btnMASTERLIST;
+        private System.Windows.Forms.Button btnREGISTER;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel Sidepanel2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+
     }
 }
 
--- BFP-FSES/Form1.cs
+++ BFP-FSES/Form1.cs
@@ -15,5 +15,17 @@ namespace BFP_FSES
         {
             InitializeComponent();
         }
+
+        private void btnREGISTER_Click(object sender, EventArgs e)
+        {
+            Sidepanel1.BackColor = Color.FromArgb(120, 40, 31);
+            Sidepanel2.BackColor = Color.FromArgb(39, 55, 70);
+        }
+
+        private void btnMASTERLIST_Click(object sender, EventArgs e)
+        {
+            Sidepanel1.BackColor = Color.FromArgb(39, 55, 70);
+            Sidepanel2.BackColor = Color.FromArgb(120, 40, 31);
+        }
     }
 }
